6852df5770e303a36b83ec7e4757d39eae7bc276
victor <flores.vicf@gmail.com>
2021-09-08 00:40:25 -0500

    Initial Commit

---
+++ Shared/foodHbApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodHbApp.swift
+//  Shared
+//
+//  Created by victor flores on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct foodHbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
